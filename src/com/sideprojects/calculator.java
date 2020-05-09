4006a3c336d855cf6d30fa7d9b3999d75e0c18d4
kingpush37 <57881553+kingpush37@users.noreply.github.com>
2020-05-09 10:39:21 -0700

    started mortgage formula

--- src/com/sideprojects/calculator.java
+++ src/com/sideprojects/calculator.java
@@ -2,6 +2,19 @@ package com.sideprojects;
 
 
 public class calculator {
+    private int monthlyPayment;
+    private long principal;
+    private double interestRate;
+    private int numOfPayments;
+
+    public void calculateMortgage() {
+        interestRate /= 12;
+        numOfPayments *= 12;
+
+        monthlyPayment = (int) ((int) (principal * (interestRate * (1 + interestRate) * numOfPayments )) /
+                        (principal * (interestRate * (1 + interestRate) * numOfPayments - 1)));
+    }
+
 
 }
 
