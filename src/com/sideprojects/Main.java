a051ca4c045f11d055e014dbe900fe6ab21d6e69
kingpush37 <57881553+kingpush37@users.noreply.github.com>
2020-05-09 08:53:06 -0700

    base files

---
+++ src/com/sideprojects/Main.java
@@ -0,0 +1,8 @@
+package com.sideprojects;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/sideprojects/calculator.java
@@ -0,0 +1,4 @@
+package com.sideprojects;
+
+public class calculator {
+}
