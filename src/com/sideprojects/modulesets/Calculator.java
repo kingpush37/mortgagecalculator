5f5ab2e90699d3b4a7bf857e98b05d6caeabb40e
kingpush37 <57881553+kingpush37@users.noreply.github.com>
2020-05-13 19:07:40 -0700

    added currency format & Interface

--- src/com/sideprojects/modulesets/Calculator.java
+++ src/com/sideprojects/modulesets/Calculator.java
@@ -1,12 +1,15 @@
 package com.sideprojects.modulesets;
 
 
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Calculator {
    private int principal;
    private double interestRate;
    private short numOfPayments;
    private double monthlyPayment;
+   private String formatMonthlyPayment;
 
 
 
@@ -18,25 +21,32 @@ public class Calculator {
 
         monthlyPayment = (int) (principal * ((rawLoanInterest * exponent) /
                         (exponent - 1)));
+
+        var locale = new Locale("en", "US");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+        formatMonthlyPayment = currencyFormatter.format(monthlyPayment);
     }
 
     public void printMortgagePayment() {
         System.out.println("----------------");
         System.out.println("Your Mortgage Payment Information");
         System.out.println(" ");
-        System.out.println("Initial Monthly Payments: " + monthlyPayment);
+        System.out.println("Initial Monthly Payments: " + formatMonthlyPayment);
         System.out.println("Total of " + numOfPayments * 12 + " payments");
     }
 
     public void setPrincipal(int principal) {
+
         this.principal = principal;
     }
 
     public void setInterestRate(double interestRate) {
+
         this.interestRate = interestRate;
     }
 
     public void setNumOfPayments(short numOfPayments) {
+
         this.numOfPayments = numOfPayments;
     }
 }
